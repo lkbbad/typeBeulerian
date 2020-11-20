b000296aa77baa9ff57aa555089b0808031791e8
aidanoneill3776 <aidanoneill3776@gmail.com>
2020-11-20 10:35:14 -0500

    recursion opt

--- marriedlife.py
+++ marriedlife.py
@@ -1,18 +1,25 @@
 from numpy.polynomial import polynomial as P
 
+posPNXDict = {}
+negPNXDict = {}
+
 
 def posPNX(n):
     if n == 1:
         return (1)
     if n < 1:
         return
+    if n in posPNXDict:
+        return posPNXDict[n]
     first = P.polymul((1, 2*n - 2), posPNX(n-1))
     second = P.polymul((0, 4), (1, -1))
     deriv = P.polyder(posPNX(n-1))
     second = P.polymul(second, deriv)
     third = P.polymul((0, 1), (negPNX(n-1)))
     ret = P.polyadd(first, second)
-    return P.polyadd(ret, third)
+    ret = P.polyadd(ret, third)
+    posPNXDict[n] = ret
+    return ret
 
 
 def negPNX(n):
@@ -20,15 +27,20 @@ def negPNX(n):
         return (1)
     if n < 1:
         return
+    if n in negPNXDict:
+        return negPNXDict[n]
     first = P.polymul((3, 2*n - 4), negPNX(n-1))
     second = P.polymul((0, 4), (1, -1))
     deriv = P.polyder(negPNX(n-1))
     second = P.polymul(second, deriv)
     third = posPNX(n-1)
     ret = P.polyadd(first, second)
-    return P.polyadd(ret, third)
+    ret = P.polyadd(ret, third)
+    negPNXDict[n] = ret
+    return ret
 
 
-for i in range(1, 5):
-    print(posPNX(i))
-    print(negPNX(i))
+for i in range(20):
+    print(posPNX(20-i))
+    print(negPNX(20-i))
+    print(P.polyadd(posPNX(20-i), negPNX(20-i)))
