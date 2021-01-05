8666e88ba5cdba8102bdc74baaf745f3512affa5
Lindy Bustabad <libustabad@davidson.edu>
2021-01-05 09:42:51 -0500

    cleaned up

--- chowmapolys.py
+++
@@ -1,8 +0,0 @@
-from numpy.polynomial import polynomial as np
-
-p_1_x_pos = (1)
-p_1_x_neg = (1)
-p_1_x_pos_d = np.Polynomial(np.polyder(p_1_x_pos))
-p_1_x_neg_d = np.Polynomial(np.polyder(p_1_x_neg))
-
-p_2_x_pos = 
